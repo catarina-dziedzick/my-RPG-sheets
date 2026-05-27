a5d9d3c086072a613c59320afb6394fc469d5f45
Catarina Dziedzick <catarina.dziedzick@totvs.com.br>
2026-05-27 10:07:24 -0300

    Initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/com/myRPG/room/MyRpgSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.myRPG.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRpgSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRpgSheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myRPG/room/MyRpgSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myRPG.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRpgSheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
